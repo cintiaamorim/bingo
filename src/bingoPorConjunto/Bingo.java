df892a85a1e5176bd5ed5b9894d862d2e85cbbfd
Cintia Amorim <cintia.amorim@outlook.com>
2022-07-02 20:10:31 -0300

    Bingo implementado utilizando conjunto

---
+++ src/bingoPorConjunto/Bingo.java
@@ -0,0 +1,65 @@
+package bingoPorConjunto;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Bingo {
+	
+	boolean maxLista = false;
+	
+	Integer numeroSorteado;
+	
+	Random aleatorio = new Random();
+	
+	Set<Integer> conjunto = new TreeSet<Integer>();
+	
+	public void sortearNumero() 
+	{			
+			maxLista = verificarLimite(false);
+			if(maxLista == true) {
+				System.out.println("Todos os números possíveis já foram sorteados");
+			}
+			else if(maxLista == false) {
+				numeroSorteado = aleatorio.nextInt(5) + 1;
+				verificarNumeroRepetido(numeroSorteado);
+			}
+
+		}
+	
+	public boolean verificarLimite(boolean checkLimite) {
+		if(conjunto.size() == 5) {
+			return true;
+	}
+			return false;
+	}
+	
+	public void verificarNumeroRepetido(Integer numeroSorteado) {
+		if(conjunto.isEmpty()) {
+			exibirNumeroSorteado();
+		} 
+		else if(!conjunto.contains(numeroSorteado)) {
+			exibirNumeroSorteado();				
+		} else {
+			sortearNumero();
+		}
+	}
+	
+	public void exibirNumeroSorteado() {
+		conjunto.add(numeroSorteado);
+		System.out.println("Número sorteado:" + numeroSorteado);
+		
+	}
+
+	
+	public void exibirTodosNumerosSorteados() {
+		System.out.println("Fim do Sorteio");
+		if(conjunto.isEmpty()) {
+			System.out.println("Nenhum número foi sorteado");
+		} else if(!conjunto.isEmpty()) {
+		System.out.println("Foram sorteados " + conjunto.size() + " número(s)");
+		System.out.println("Os números sorteados são: " + conjunto.toString());
+		}
+		}
+
+}
---
+++ src/bingoPorConjunto/Sorteio.java
@@ -0,0 +1,33 @@
+package bingoPorConjunto;
+
+import java.util.Scanner;
+
+public class Sorteio {
+		
+		public static void main(String[] args) {
+			
+			Scanner entrada = new Scanner(System.in);
+			
+			String letra;
+
+			Bingo bingo = new Bingo();	
+			
+			do{
+				System.out.println("Digite a letra (R para sortear, X para sair)");
+				letra = entrada.next();
+				if(letra.equalsIgnoreCase("X")) {				
+					break;
+				}
+				bingo.sortearNumero(); 
+				if(bingo.maxLista == true) {
+					break;			
+				}
+			}while(letra.equalsIgnoreCase("R"));		
+	  
+	        bingo.exibirTodosNumerosSorteados();
+
+
+
+	}
+
+}
